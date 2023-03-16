df84a2f85fb1747c9f8bc7783f625fe29400a42d
Jonah <jonahemmanuel54@gmail.com>
2023-03-16 12:16:39 -0700

    put.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+/**
+ * main - Entrt point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+	puts("programming is like building a multilingual language");
+	return (0);
+}
