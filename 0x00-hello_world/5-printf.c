cafd4d51f954f744d091358fe70e137a86af6b70
Jonah <jonahemmanuel54@gmail.com>
2023-03-16 12:33:32 -0700

    printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+/**
+ * main - Entrt point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+	printf("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
+
