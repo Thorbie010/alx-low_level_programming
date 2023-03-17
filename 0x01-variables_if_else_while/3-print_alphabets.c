971de7cae37a1d3e1a89fae06a1906bdc469f7eb
Jonah <jonahemmanuel54@gmail.com>
2023-03-17 12:18:21 -0700

    ABCDE

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,14 +8,11 @@
 int main(void)
 {
 
-        char x;
-
-        for (x = 'a'; x <= 'z'; x++)
-        {
-                putchar(x);
-        }
-                putchar('\n');
-
+	char x;
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		putchar(x);
+	}
 	for (x = 'A'; x <= 'Z'; x++)
 	{
 		putchar(x);
