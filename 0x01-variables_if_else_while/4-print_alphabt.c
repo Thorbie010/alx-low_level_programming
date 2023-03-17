0ddb1b121c3f9504e4eb2565ad2f2be3f609a481
Jonah <jonahemmanuel54@gmail.com>
2023-03-17 16:14:53 -0700

    hhgh

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+
+/**
+ * main - Entry point
+ *
+ * * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	char x, q, e;
+
+	e = 'e';
+	q = 'q';
+
+	for (x = 'a'; x <= 'z'; x++)
+		if (x != e && x != q)
+		{
+			putchar(x);
+		}
+		putchar('\n');
+
+	return (0);
+}
