3939be350d7478222bd42d92e0c7fd0e2ad96d4e
Jonah <jonahemmanuel54@gmail.com>
2023-03-17 19:19:57 -0700

    combinations of 2digits ommiting repetition

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * printing possible combinations of 2 digit while skipping repetition
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (b = a + 1; b < '9'; b++)
+		{
+			if (a != b)
+			{
+				putchar(a);
+				putchar(b);
+
+				if (a == '8' && b == '9')
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
