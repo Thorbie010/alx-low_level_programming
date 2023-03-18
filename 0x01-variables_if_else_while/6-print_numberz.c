91eea21baee0d9fc889ca91d73f4e55311ac3a7a
Jonah <jonahemmanuel54@gmail.com>
2023-03-17 17:26:58 -0700

    0-9 whithout char using only putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * printing 0-9 without using char
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	for (int r = '0';  r <= '9'; r++)
+	{
+		putchar(r);
+	}
+	putchar('\n');
+	return (0);
+}
