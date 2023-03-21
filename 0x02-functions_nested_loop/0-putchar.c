9926747cb17f93ff6ea12470b2b6ac20425464a0
Jonah <jonahemmanuel54@gmail.com>
2023-03-21 01:18:13 -0700

    program the prints _putchar

---
+++ 0x02-functions_nested_loop/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * program that writes putcharbfollowed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char str[9] = "_putchar";
+
+	puts(str);
+
+	return (0);
+}
