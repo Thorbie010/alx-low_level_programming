53397668e5cceb04536b3203a903f19af0432f9e
Jonah <jonahemmanuel54@gmail.com>
2023-03-22 11:05:01 -0700

    natural numbers, fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: This program prints the sum of the even valued terms
+ * in the Fibonacci sequence
+ * Return: 0  (Success)
+ */
+
+int main(void)
+{
+	unsigned long int a = 1;
+	unsigned long int b = 2;
+	unsigned long int next;
+	unsigned long int i;
+	unsigned long int sum = 0;
+
+	for (i = 0; i <= 50; i++)
+	{
+		a = b;
+		b = next;
+		next = a + b;
+
+		if (next % 2 == 0 && next < 4000000)
+		{
+			sum = sum + next;
+		}
+	}
+
+	printf("%lu\n", sum);
+
+	return (0);
+}
