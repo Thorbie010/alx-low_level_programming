c67f63ab6f6b02173c8a35a07fa7342c0d0aaced
Jonah <jonahemmanuel54@gmail.com>
2023-03-21 19:02:00 -0700

    checking foruppercase or lower case letters

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower (int c) - start here
+ * program to check if c is a lowercase or uppercase letter
+ * Return: nothing
+ */
+int _islower(int c)
+{
+
+	if (_islower(c))
+		return (1);
+	else
+		return (0);
+}
