b07f5d465af1411520bb895bb19a380f3202ca6a
Jonah <jonahemmanuel54@gmail.com>
2023-03-22 11:11:32 -0700

    printing first 50  fibonacci numbers

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,27 +1,28 @@
 #include <stdio.h>
 
 /**
- * main - Entry Point
+ * main - Entry point
  *
- * Description: This program prints the sum of all the multiples of 3 and 5
- * below 1024
- * Return: 0 (Success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
 	int i;
-	int sum = 0;
+	long int fibonacci[50];
 
-	for (i = 0; i < 1024; i++)
+	fibonacci[0] = 1;
+	fibonacci[1] = 2;
+	printf("%ld, %ld, ", fibonacci[0], fibonacci[1]);
+
+	for (i = 2; i < 50; i++)
 	{
-		if ((i % 3 == 0) || (i % 5 == 0))
-		{
-			sum = sum + i;
-		}
+		fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+		if (i == 49)
+			printf("%ld\n", fibonacci[i]);
+		else
+			printf("%ld, ", fibonacci[i]);
 	}
-
-	printf("%d\n", sum);
 
 	return (0);
 }
