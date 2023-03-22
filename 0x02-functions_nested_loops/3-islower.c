69cef58670c9ea9165f8dc24b5d19de7f97808fe
Jonah <jonahemmanuel54@gmail.com>
2023-03-21 20:05:27 -0700

    uppercase or lowercase letter

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,14 +4,16 @@
  * _islower - start here
  * @c: check c
  * program to check if c is a lowercase or uppercase letter
- * Return: 1 or 0
+ * Return: 1 or 0 (Success)
  */
 int _islower(int c)
 {
 
 
 	if (c >= 97 && c <= 122)
+	{
 		return (1);
+	}
 	else
 		return (0);
 }
