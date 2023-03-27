6d9e3177648df19284071de9714479cacf073978
Jonah <jonahemmanuel54@gmail.com>
2023-03-27 10:14:39 -0700

    reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _puts - Write a function that prints a string, followed
+ * by a new line, to stdout.
+ * @str: an input string
+ * Return: Nothing
+ */
+void print_rev(char *s)
+{
+	int i = 0, j = 0;
+	char rev;
+
+	while (s[i] != '\0')
+		i++
+	
+	while (j < i--)
+	{
+		rev = s[j];
+		s[j++] = s[i];
+		s[i] = rev;
+	}
+}
