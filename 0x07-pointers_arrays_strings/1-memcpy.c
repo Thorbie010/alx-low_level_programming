67ab56c2da4a5a4de30702faa5a6a706a8f7403d
Jonah <jonahemmanuel54@gmail.com>
2023-04-03 16:56:50 -0700

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _memcpy - function to copy one variable into another
+ * @dest: 1st variable
+ * @src: 2nd variable
+ * @n: number of times
+ * Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char tmp;
+	unsigned int i;
+
+	tmp = dest
+	i = 0
+
+	for (; i < n; i++)
+	{
+		*tmp++ = *src++;
+	}
+	return (dest);
+
+
+}
