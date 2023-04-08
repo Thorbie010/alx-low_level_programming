43a41486442b4d3b9b4a769c47a761f750108c62
Jonah <jonahemmanuel54@gmail.com>
2023-04-08 14:02:35 -0700

    argc argv addition

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+/**
+ * main - program that adds argument values
+ * @argc: number of arguments counted
+ * @argv: argument values
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, add = 0;
+
+	while (argc-- > 1)
+	{
+		for (i = 0; i < argv[argc][i]; i++)
+		{
+			if (isdigit(argv[argc][i]))
+			{
+				add += atoi(argv[argc]);
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+	}
+	printf("%d\n", add);
+	return (0);
+}
