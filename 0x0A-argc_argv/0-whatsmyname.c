f65147513bebc5c805b322a8f7e630c6bd50a991
Jonah <jonahemmanuel54@gmail.com>
2023-04-09 07:11:21 -0700

    argc argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that prints number of arguments
+ * @argc: number of arguments counted
+ * @argv: argument strings
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	int i;
+
+	i = 0;
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
+}
