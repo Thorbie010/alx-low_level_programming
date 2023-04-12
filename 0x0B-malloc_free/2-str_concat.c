bd9c898a7785fa9b984b8bc02ed5dbf27a397197
Jonah <jonahemmanuel54@gmail.com>
2023-04-12 06:30:19 -0700

    malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,11 +14,10 @@ char *str_concat(char *s1, char *s2)
 {
 	char *new_str;
 
-	if (s1 == NULL || s2 == NULL)
-	{
+	if (s1 == NULL)
 		s1 = "";
+	if (s2 == NULL)
 		s2 = "";
-	}
 
 	new_str = (char *) malloc(((strlen(s1)) + (strlen(s2))) + 1);
 	if (new_str == NULL)
