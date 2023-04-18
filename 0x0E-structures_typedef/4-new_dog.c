dd67d1da4c1668875deecdea8737e55d928b5802
Jonah <jonahemmanuel54@gmail.com>
2023-04-18 00:51:08 -0700

    structure definitions

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,15 +10,44 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *New_dog;
+	dog_t *new_dog;
+	char *cpyname, *cpyowner;
+	int len_name = 0, len_owner = 0, i;
 
-	New_dog = malloc(sizeof(dog_t));
-	if (New_dog == NULL)
+	if (name == NULL || owner == NULL)
 		return (NULL);
 
-	New_dog->name = name;
-	New_dog->age = age;
-	New_dog->owner = owner;
+	while (name[len_name])
+		len_name++;
+	while (owner[len_owner])
+		len_owner++;
 
-	return (New_dog);
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+		return (NULL);
+
+	cpyname = malloc(len_name + 1);
+	if (cpyname == NULL) {
+		free(new_dog);
+		return (NULL);
+	}
+	for (i = 0; name[i]; i++)
+		cpyname[i] = name[i];
+	cpyname[i] = '\0';
+
+	cpyowner = malloc(len_owner + 1);
+	if (cpyowner == NULL) {
+		free(cpyname);
+		free(new_dog);
+		return (NULL);
+	}
+	for (i = 0; owner[i]; i++)
+		cpyowner[i] = owner[i];
+	cpyowner[i] = '\0';
+
+	new_dog->name = cpyname;
+	new_dog->age = age;
+	new_dog->owner = cpyowner;
+	return (new_dog);
 }
+
