845f306c97fee3d90e6f0f00c358d6fdd9f9e203
Jonah <jonahemmanuel54@gmail.com>
2023-04-18 00:21:15 -0700

    structure definitions

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,10 +13,4 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-};
-typedef struct dog dog_h;
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-#endif
+}dog;
