1bd3f12681d96e31fdef69dcacfbc7d6683d55b9
Jonah <jonahemmanuel54@gmail.com>
2023-04-18 01:03:32 -0700

    structure definitions

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,5 @@
 #include "dog.h"
 #include <stdlib.h>
-
 /**
  * new_dog - creates a new dog structure
  * @name: Dog's name
@@ -16,28 +15,23 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (name == NULL || owner == NULL)
 		return (NULL);
-
 	while (name[len_name])
 		len_name++;
 	while (owner[len_owner])
 		len_owner++;
-
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
-
 	cpyname = malloc(len_name + 1);
-	if (cpyname == NULL) 
-	{
-		free(new_dog);
+	if (cpyname == NULL)
+	{	free(new_dog);
 		return (NULL);
 	}
 	for (i = 0; name[i]; i++)
 		cpyname[i] = name[i];
 	cpyname[i] = '\0';
-
 	cpyowner = malloc(len_owner + 1);
-	if (cpyowner == NULL) 
+	if (cpyowner == NULL)
 	{
 		free(cpyname);
 		free(new_dog);
@@ -46,10 +40,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	for (i = 0; owner[i]; i++)
 		cpyowner[i] = owner[i];
 	cpyowner[i] = '\0';
-
 	new_dog->name = cpyname;
 	new_dog->age = age;
 	new_dog->owner = cpyowner;
 	return (new_dog);
 }
-
