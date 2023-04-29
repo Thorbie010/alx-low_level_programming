ff9f9ac6e745314ff2628aa2cef6691e22f963ca
Jonah <jonahemmanuel54@gmail.com>
2023-04-29 06:03:38 -0700

    more singly linked list

--- 0x13-more_singly_linked_lists/list.h
+++ 0x13-more_singly_linked_lists/list.h
@@ -1,7 +1,16 @@
 #ifndef LIST_H
 #define LIST_H
 
-typedef struct listint_s {
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * 
+ */
+typedef struct listint_s
+{
     int n;
     struct listint_s *next;
 } listint_t;
