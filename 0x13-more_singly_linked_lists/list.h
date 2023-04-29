b43f72a0ffd83da2a7d958da1023ed487ae9f5c1
Jonah <jonahemmanuel54@gmail.com>
2023-04-29 05:52:12 -0700

    more singly linked list

---
+++ 0x13-more_singly_linked_lists/list.h
@@ -0,0 +1,11 @@
+#ifndef LIST_H
+#define LIST_H
+
+typedef struct listint_s {
+    int n;
+    struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+
+#endif /* LIST_H */
