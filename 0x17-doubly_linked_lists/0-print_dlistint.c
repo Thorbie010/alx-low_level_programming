6320c5ea93ca3837d3f80de0ea6fa0ea53480af5
Jonah <jonahemmanuel54@gmail.com>
2023-06-15 08:23:11 -0700

    doubly linked lists

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_dlistint -
+ * @h:
+ * Return:
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *current = h;
+	size_t nodeCount = 0;
+	
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+		nodeCount++;
+	}
+	return (nodeCount);
+}
