283baa4f88b70a1caf17664657d70172e95ef2da
Jonah <jonahemmanuel54@gmail.com>
2023-07-07 05:39:53 -0700

    adding item to hash table

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+#include <string.h>
+/**
+ * hash_table_set - function that adds element to the hash table
+ * @ht: hass table to be updated
+ * @key: is the key
+ * @value: value associated with the key
+ * Return: 1 if success otherwise return 0
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	int index = key_index((unsigned char *)key, ht->size);
+	hash_node_t *newPair;
+	char *new_key = strdup(key), *new_value = strdup(value);
+
+	newPair = (hash_node_t *)malloc(sizeof(hash_node_t));
+	if (newPair == NULL)
+		return (0);
+
+	newPair->key = new_key;
+	newPair->value = new_value;
+
+	if (ht->array[index] != NULL)
+	{
+		hash_node_t *current = ht->array[index];
+
+		while (current->next != NULL)
+			current = current->next;
+		current->next = newPair;
+	}
+	else
+		ht->array[index] = newPair;
+	return (1);
+}
