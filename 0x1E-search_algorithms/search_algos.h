60dc7a5617ca60adffc6587707d4b6a12fe01f22
Jonah <jonahemmanuel54@gmail.com>
2023-10-11 02:34:04 -0700

    fixing task 0

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,6 +1,10 @@
 #ifndef _SEARCH_ALGOS_H_
 #define _SEARCH_ALGOS_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
 int linear_search(int *array, size_t size, int value);
 
 #endif 
